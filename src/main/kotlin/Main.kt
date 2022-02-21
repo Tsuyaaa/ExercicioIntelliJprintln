289731c8b35ff9d111665f7b89fe7f0fafdcf4f0
Tsuyaaa <imtsuya@gmail.com>
2022-02-21 16:21:23 -0300

    Texto feito em grupo

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println("Jose: I aew Jão, d boas?")
+    println("Dj Rogerio: D boas man e tu?")
+    println("Jose: Então mano, tava afim d ouvir aquela braba ta ligado???")
+    println("Dj Rogerio: Qual mano, não tenho bola de cristal!!!")
+    println("Jose: Pega leve chapa, fiko desumilde???")
+    println("Dj Rogerio: Não mano, vc chega apavorando, como vou saber?")
+    println("Jose: Malz ae entao, tava afim de ouvir Cleiton Rasta pod pah?")
+    println("Dj Rogerio: Gostei da ideia, acabando essa ja dou o play")
+    println("Jose: Então mano, pensei melhor, nao quero mais Cleiton Rasta!!!")
+    println("Dj Rogerio: Po mano, como assim, ja ta de sacanagem com a minha face!!! Se decide Jão!!!")
+    println("Jose: Não me apressa to pensando.")
+    println("Dj Rogerio: (DJ Rogerio sacou um facão, e meteu um apavoro) Coé, vai decidir NÃOOOOO!!! ")
+    println("Jose: Coé moh stresss, soh queria curtir a vibe mas desse jeito fiko de caozada!!!")
+    println("Dj Rogerio: Sai andando então, arrumo nada!")
+    println("Jose: Já eh Rogerin nao queria ouvir mesmo, tó nem manda bem!!!!")
+}
